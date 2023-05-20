fbe0e0dd02c5231dd81a9939e78cc59b9aa69ddf
Ojochogwu Dickson <70067819+Ojochogwu866@users.noreply.github.com>
2023-05-20 15:56:56 +0100

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -9,7 +9,7 @@ const {
   getAllData,
 } = require("../controllers/data");
 
-router.route("/create-data").post(creategasData);
+router.route("create-data").post(creategasData);
 router.route("/data/:deviceId").get(getAllData);
 /*router.route("/data/:deviceId/:dataId").put(updateGasData);
 router.route("/data/:deviceId/:dataId").delete(deleteGasData);
